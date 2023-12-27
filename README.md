a97a1c4f0703f928eed7055fb541ce6011d32d38
Guille Rodríguez <guille.rodriguez.gonzalez@gmail.com>
2023-12-27 19:32:46 +0100

    Migrated from https://github.com/guillerg86/zabbix/tree/main/configs/tplink-sg108e

--- README.md
+++ README.md
@@ -1,2 +1,327 @@
-# zabbix-template-sg108e
-Template and scripts for monitoring TPLink TL-SG108E with Zabbix
+## Template TP-Link SG108E
+
+This template allows to monitor TP-Link SG108E switch. This switch is a home switch, so doesn't have SNMP or SSH, just a website.
+
+Check images folder for examples
+
+
+### Items
+
+- Hostname
+- Firmware
+- Hardware
+- Mac Address
+
+### Triggers
+
+- Hostname changed
+- Firmware changed
+- Hardware changed
+- MAC address changed
+
+### Discovery rules
+
+There's a discovery rule for detect network ports. When executed, it returns a json with port number and adminstatus
+
+```
+{
+    "interfaces": [
+        {
+			"{#JSON.INDEX}": 0,
+            "{#IFNUMBER}": 1,
+            "{#IFADMINSTATUS}": "1"
+        },
+        {
+			"{#JSON.INDEX}": 1,
+            "{#IFNUMBER}": 2,
+            "{#IFADMINSTATUS}": "0"
+        },
+        {
+			"{#JSON.INDEX}": 2,
+            "{#IFNUMBER}": 3,
+            "{#IFADMINSTATUS}": "0"
+        },
+        {
+			"{#JSON.INDEX}": 3,
+            "{#IFNUMBER}": 4,
+            "{#IFADMINSTATUS}": "0"
+        },
+        {
+			"{#JSON.INDEX}": 4,
+            "{#IFNUMBER}": 5,
+            "{#IFADMINSTATUS}": "1"
+        },
+        {
+			"{#JSON.INDEX}": 5,
+            "{#IFNUMBER}": 6,
+            "{#IFADMINSTATUS}": "1"
+        },
+        {
+			"{#JSON.INDEX}": 6,
+            "{#IFNUMBER}": 7,
+            "{#IFADMINSTATUS}": "1"
+        },
+        {
+			"{#JSON.INDEX}": 7,
+            "{#IFNUMBER}": 8,
+            "{#IFADMINSTATUS}": "1"
+        }
+    ]
+}
+```
+
+Only creates Item prototypes when {#IFADMINSTATUS} = 1 (enabled)
+
+#### Item prototypes 
+
+- Admin status
+- Operational status
+- Flow control actual
+- Flow control configuration
+- Received packets
+- Received packets error
+- Transmitted packets
+- Transmitted packets error
+- Trunk status
+- Speed configuration
+- Speed actual
+- Speed (bps)
+
+#### Trigger prototypes
+
+- Interface: Link down
+- Interface: Link speed is lower than before
+- Interface: Flow control changed
+- Interface: Speed configuration changed
+
+## Tested on
+
+- TP-Link SG108E v6.0 
+  - Firmware: 20230218 ✔
+  - Firmware: 20201208 ✔
+
+## Test script before install on Zabbix
+
+If you want to test before install on Zabbix, you need a machine with Python 3.x installed (tested on 3.12).
+
+**Get System Info**
+
+```
+python3 tplinkswitch2zabbix.py -i IP_ADDRESS -u WEBUSERNAME -p WEBPASSWORD -a sysinfo
+```
+
+Output be like:
+
+```
+{
+    "firmware": "1.0.0 Build 20230218 Rel.50633",
+    "hardware": "TL-SG108E 6.0",
+    "description": "PSC-SW-SG108E",
+    "mac_address": "00:31:92:B3:EE:12",
+    "ip_address": "10.15.1.2",
+    "netmask": "255.255.255.0",
+    "gateway": "10.15.1.1",
+    "port_number": 0,
+    "ports": []
+}
+```
+
+This method only takes system information, no ports information.
+
+**Discovery ports**
+
+```
+python3 tplinkswitch2zabbix.py -i IP_ADDRESS -u WEBUSERNAME -p WEBPASSWORD -a discovery
+```
+
+Output be like:
+
+```
+{
+    "interfaces": [
+        {
+            "{#JSON.INDEX}": 0,
+            "{#IFNUMBER}": 1,
+            "{#IFADMINSTATUS}": "1"
+        },
+        {
+            "{#JSON.INDEX}": 1,
+            "{#IFNUMBER}": 2,
+            "{#IFADMINSTATUS}": "1"
+        },
+		....
+		....
+        {
+            "{#JSON.INDEX}": 7,
+            "{#IFNUMBER}": 8,
+            "{#IFADMINSTATUS}": "1"
+        }
+    ]
+}
+```
+
+**Port detail**
+
+```
+python3 tplinkswitch2zabbix.py -i IP_ADDRESS -u WEBUSERNAME -p WEBPASSWORD -a portinfo --port-number 8
+```
+
+Output be like
+
+```
+{
+    "port_number": 8,
+    "admin_status": "1",
+    "trunk_status": "0",
+    "operation_status": 1,
+    "speed_config": "1",
+    "speed_actual": "6",
+    "speed_bps": 1000000000,
+    "flowcontrol_config": "0",
+    "flowcontrol_actual": "0",
+    "transmitted_packets": "324937",
+    "transmitted_packets_error": "0",
+    "received_packets": "615998",
+    "received_packets_error": "708"
+}
+```
+
+**All Info**
+
+```
+python3 tplinkswitch2zabbix.py -i IP_ADDRESS -u WEBUSERNAME -p WEBPASSWORD -a allinfo
+```
+
+Output be like 
+
+```
+{
+    "firmware": "1.0.0 Build 20230218 Rel.50633",
+    "hardware": "TL-SG108E 6.0",
+    "description": "PSC-SW-SG108E",
+    "mac_address": "00:31:92:B3:EE:12",
+    "ip_address": "10.15.1.2",
+    "netmask": "255.255.255.0",
+    "gateway": "10.15.1.1",
+    "port_number": 8,
+    "ports": [
+        {
+            "port_number": 1,
+            "admin_status": "1",
+            "trunk_status": "0",
+            "operation_status": 0,
+            "speed_config": "1",
+            "speed_actual": "0",
+            "speed_bps": 0,
+            "flowcontrol_config": "0",
+            "flowcontrol_actual": "0",
+            "transmitted_packets": "0",
+            "transmitted_packets_error": "0",
+            "received_packets": "0",
+            "received_packets_error": "0"
+        },
+		....
+		....
+        {
+            "port_number": 8,
+            "admin_status": "1",
+            "trunk_status": "0",
+            "operation_status": 1,
+            "speed_config": "1",
+            "speed_actual": "6",
+            "speed_bps": 1000000000,
+            "flowcontrol_config": "0",
+            "flowcontrol_actual": "0",
+            "transmitted_packets": "346961",
+            "transmitted_packets_error": "0",
+            "received_packets": "540128",
+            "received_packets_error": "504"
+        }
+    ]
+}
+```
+
+## Installation
+
+You can monitor this switch using agent or server/proxy (external script). 
+
+### Macros
+
+|MACRO|DESCRIPTION|
+|-|-|
+|{$SW.IP}|IP ADDRESS of the Switch|
+|{$SW.USER}|Switch Webadmin user|
+|{$SW.PASS}|Switch Webadmin password|
+
+### By Zabbix agent
+
+- Import template `Template TP-Link SG108E Webparsing by Zabbix Agent.yaml` to your zabbix server.
+- Create a folder inside zabbix-agent folder.
+
+```
+mkdir /etc/zabbix/scripts
+```
+
+- Download script `tplinkswitch2zabbix.py` and copy to scripts folder
+
+```
+cp tplinkswitch2zabbix.py /etc/zabbix/scripts/
+chmod +x /etc/zabbix/scripts/tplinkswitch2zabbix.py
+```
+- Download `tplinkswitch.conf` and copy to `/etc/zabbix/zabbix_agentd.d/` or `/etc/zabbix/zabbix_agent2.d/`.
+- Reboot agent with
+
+```
+systemctl restart zabbix_agentd.service
+```
+or
+
+```
+systemctl restart zabbix_agent2.service
+```
+
+- Create a host on Zabbix with agent and asigng the Template `Template TP-Link SG108E Webparsing by Zabbix Agent.yaml`
+- Inside host set macros values
+- If host is monitored by proxy (wait, resync or restart proxy)
+- Test and confirm
+
+
+### By Zabbix Server / Proxy (external script)
+
+External scripts are scripts executed by zabbix server or proxy (not by agent), so no need to copy UserParameters config. 
+
+- Import template `Template TP-LINK SG108E WebParsing by Zabbix Server or Proxy.yaml` to your zabbix server.
+- Check the path of ExternalScripts on your Zabbix Server or Zabbix Proxy config (default is on `/usr/lib/zabbix/externalscripts`)
+
+For server
+
+```
+cat /etc/zabbix/zabbix_server.conf | grep ExternalScripts
+```
+
+For proxy
+
+```
+cat /etc/zabbix/zabbix_proxy.conf | grep ExternalScripts
+```
+
+- Download python script and copy to ExternalScripts path
+
+```
+cp tplinkswitch2zabbix.py /usr/lib/zabbix/externalscripts/
+chmod +x /usr/lib/zabbix/externalscripts/tplinkswitch2zabbix.py
+```
+
+- Create a host, add template `Template TP-LINK SG108E WebParsing by Zabbix Server or Proxy`
+- Inside host set macros values
+- If host is monitored by proxy (wait, resync or restart proxy)
+- Test and confirm
+
+
+## "Problems detected"
+
+This switch only allows some concurrent sessions on web management, so when this script connects to retrieve data this will force a logout on your web session if you are connected. You can do one of both things.
+
+- Install TP-Link Easy Smart Configuration Utility (you can manage switch without getting disconnected)
+- Stop monitoring this switch while you are configuring the switch via web.
+
---
+++ tplinkswitch2zabbix.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python3
+#######################################################################################
+# @author: Guille Rodriguez https://github.com/guillerg86
+# @version: 2023-12-18 01:30
+# @python-version: 3.x
+#
+# Script for monitor with Zabbix device like TL-SG108E (which doesn't have SNMP)
+# just web. Care, if there are other users connected, when this script connect
+# maybe one of them will be session disconnected (logout).
+#
+# Tested on:
+# - TL-SG108E v6.0 with 20201208 and 20230218 firmware
+#######################################################################################
+import json
+import re
+import requests
+import argparse
+from ipaddress import ip_address
+
+class SwitchPort:
+    def __init__(self):
+        self.port_number = 0
+        self.admin_status = 0
+        self.trunk_status = 0
+        self.operation_status = 0
+        self.speed_config = 0
+        self.speed_actual = 0
+        self.speed_bps = 0
+        self.flowcontrol_config = 0
+        self.flowcontrol_actual = 0
+        self.transmitted_packets = 0
+        self.transmitted_packets_error = 0
+        self.received_packets = 0
+        self.received_packets_error = 0
+
+
+class TPLinkSwitch:
+    def __init__(self):
+        self.firmware = None
+        self.hardware = None
+        self.description = None
+        self.mac_address = None
+        self.ip_address = None
+        self.netmask = None
+        self.gateway = None
+        self.port_number = 0
+        self.ports = []
+
+class DAOSwitchTPLink():
+
+    def __init__(self):
+        self.switch = TPLinkSwitch()
+        self.ip_address = None
+        self.username = None
+        self.password = None
+
+    def get_switch(self):
+        return self.switch
+
+    def get_base_url(self):
+        return f"http://{str(self.ip_address)}/"
+
+    def do_login(self):
+        url_login = self.get_base_url() + "logon.cgi"
+        data = {
+            'username': self.username,
+            'password': self.password,
+            'logon': 'Login'
+        }
+        session = requests.Session()
+        resp = session.post(url_login, data=data)
+        if resp.status_code == 200:
+            return session
+        else:
+            raise Exception(f"Error: HTTP Status code: {str(resp.status_code)}")
+    def load_sysinfo(self, session):
+        response = session.get(self.get_base_url() + "SystemInfoRpm.htm")
+
+        pattern_rawdata = re.compile(r"var info_ds = ({\n?(.*?)\n?});$", re.MULTILINE | re.DOTALL)
+        data = pattern_rawdata.search(response.text)
+        raw_data = data.group(1).replace("\n", "")
+
+        pattern_values = re.compile(r"[a-zA-z]+:\[\"([a-zA-Z0-9\.\s\-:]+)\"\]")
+        values = pattern_values.findall(raw_data)
+
+        self.switch.description = values[0]
+        self.switch.mac_address = values[1]
+        self.switch.ip_address = values[2]
+        self.switch.netmask = values[3]
+        self.switch.gateway = values[4]
+        self.switch.firmware = values[5]
+        self.switch.hardware = values[6]
+
+
+    def __get_portsinfo_values_from_raw(self,key,rawdata):
+        pattern = re.compile(key+":\\[([\\d,]+)\\]", re.MULTILINE | re.DOTALL)
+        return pattern.search(rawdata).group(1).split(",")
+
+    def load_portsinfo(self, session):
+
+        speed_actual_to_bps_translate_table = {
+            "0": 0,
+            "1": 0,
+            "2": 10 * 1000 * 1000,
+            "3": 10 * 1000 * 1000,
+            "4": 100 * 1000 * 1000,
+            "5": 100 * 1000 * 1000,
+            "6": 1000 * 1000 * 1000
+        }
+
+        resp_port_settings = session.get(self.get_base_url() + "PortSettingRpm.htm")
+        resp_port_monitor = session.get(self.get_base_url() + "PortStatisticsRpm.htm")
+
+        pattern_portnum = re.compile(r"var max_port_num = (\d+);")
+        self.switch.port_number = int(pattern_portnum.search(resp_port_settings.text).group(1))
+
+        pattern_allinfo = re.compile(r"var all_info = {(\n?(.*?)\n?)};", re.MULTILINE | re.DOTALL)
+        ports_config_raw = pattern_allinfo.search(resp_port_settings.text).group(1)
+
+        admin_status = self.__get_portsinfo_values_from_raw("state",ports_config_raw)
+        trunk_status = self.__get_portsinfo_values_from_raw("trunk_info",ports_config_raw)
+        speed_config = self.__get_portsinfo_values_from_raw("spd_cfg",ports_config_raw)
+        speed_actual = self.__get_portsinfo_values_from_raw("spd_act",ports_config_raw)
+        flowc_config = self.__get_portsinfo_values_from_raw("fc_cfg",ports_config_raw)
+        flowc_actual = self.__get_portsinfo_values_from_raw("fc_act",ports_config_raw)
+        packets_info = self.__get_portsinfo_values_from_raw("pkts", resp_port_monitor.text)
+        # Link status on PortStatistics is the same as speed_actual in PortSettings
+        # link_status = self.__get_portsinfo_values_from_raw("link_status", resp_port_monitor.text)
+
+        self.switch.ports = [SwitchPort() for i in range(self.switch.port_number)]
+
+        # Getting info of each port
+        for i in range(self.switch.port_number):
+            self.switch.ports[i].port_number = i+1
+            self.switch.ports[i].admin_status = admin_status[i]
+            self.switch.ports[i].trunk_status = trunk_status[i]
+            self.switch.ports[i].speed_config = speed_config[i]
+            self.switch.ports[i].speed_actual = speed_actual[i]
+            self.switch.ports[i].speed_bps = speed_actual_to_bps_translate_table[speed_actual[i]]
+            self.switch.ports[i].operation_status = 1 if int(speed_actual[i]) > 0 else 0
+            self.switch.ports[i].flowcontrol_config = flowc_config[i]
+            self.switch.ports[i].flowcontrol_actual = flowc_actual[i]
+            self.switch.ports[i].transmitted_packets = packets_info[i*4]
+            self.switch.ports[i].transmitted_packets_error = packets_info[i*4+1]
+            self.switch.ports[i].received_packets = packets_info[i*4+2]
+            self.switch.ports[i].received_packets_error = packets_info[i*4+3]
+
+
+def configure_parser():
+    """
+    Configure arguments of this script
+    :return:
+    """
+    # Configure parser
+    parser = argparse.ArgumentParser(
+        prog="TP-Link SG108E to Zabbix",
+        description="Export information from website to Zabbix"
+    )
+    parser.add_argument('-a', '--action', required=True, help="sysinfo/discovery/portinfo/allinfo",
+                        choices=["sysinfo", "discovery", "portinfo", "allinfo"])
+    parser.add_argument('-i', '--ip-address', required=True, type=ip_address)
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('--port-number',required=False, type=int, default=-1)
+    args = parser.parse_args()
+    args.action = args.action.lower()
+    return args
+
+if __name__ == '__main__':
+    args = configure_parser()
+    dao = DAOSwitchTPLink()
+
+    dao.ip_address = args.ip_address
+    dao.username = args.username
+    dao.password = args.password
+
+    if args.action == "sysinfo":
+        session = dao.do_login()
+        dao.load_sysinfo(session)
+        switch = dao.get_switch()
+        print(json.dumps(switch.__dict__,indent=4))
+    elif args.action == "discovery":
+        session = dao.do_login()
+        dao.load_portsinfo(session)
+        switch = dao.get_switch()
+        print(json.dumps({"interfaces": [ {
+            "{#JSON.INDEX}": index,
+            "{#IFNUMBER}": port.port_number,
+            "{#IFADMINSTATUS}": port.admin_status
+        } for index, port in enumerate(switch.ports)]},indent=4))
+    elif args.action == "portinfo":
+        if args.port_number <= 0:
+            print("Error: Port need to be bigger than 0. (1 <-> MAX_PORTS)")
+            exit(3)
+        session = dao.do_login()
+        dao.load_portsinfo(session)
+        switch = dao.get_switch()
+        if args.port_number > len(switch.ports):
+            print(f"Error: Port needs to be between 1 and {str(len(switch.ports))} (max-ports of this switch)")
+            exit(4)
+        # Substract 1 to number sent, port 1 is on position 0 of the array
+        port = switch.ports[args.port_number - 1]
+        print(json.dumps(port.__dict__,indent=4))
+    elif args.action == "allinfo":
+        session = dao.do_login()
+        dao.load_sysinfo(session)
+        dao.load_portsinfo(session)
+        switch = dao.get_switch()
+        # Converting object ports to dict
+        switch.ports = [vars(port) for port in switch.ports]
+        print(json.dumps(vars(switch), indent=4))
+
